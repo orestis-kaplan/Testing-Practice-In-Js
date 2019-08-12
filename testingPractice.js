7141287365f837c63e0d332c2e094436ac22e02f
userman95 <orestismail71@gmail.com>
2019-08-12 11:40:35 +0300

    adding eslint and babel functionalities

--- testingPractice.js
+++ testingPractice.js
@@ -1,94 +1,103 @@
 
-function capitalize(string){
-    return string.replace(string[0],string[0].toUpperCase());
+function capitalize(string) {
+  return string.replace(string[0], string[0].toUpperCase());
 }
 
-function reverseString(string){
-    return  string.split("").reverse().join("");
+function reverseString(string) {
+  return string.split('').reverse().join('');
 }
 
-class Calculator{
-    constructor(a,b){
-        this.a = a;
-        this.b = b;
-    }
-    add(){
-        return this.a + this.b;
-    }
-    substract(){
-        return this.a - this.b;
-    }
-    divide(){
-        return this.a / this.b;
-    }
-    multiply(){
-        return this.a * this.b;
-    }
+class Calculator {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  add() {
+    return this.a + this.b;
+  }
+
+  substract() {
+    return this.a - this.b;
+  }
+
+  divide() {
+    return this.a / this.b;
+  }
+
+  multiply() {
+    return this.a * this.b;
+  }
 }
 
-const caesarCipher = (()=>{
+const caesarCipher = (() => {
+  function translateToNums() {
+    const AllChars = [];
 
-    function translateToNums(){
-        var AllChars =[];
+    for (let i = 97; i < 123; i += 1) { AllChars.push(String.fromCharCode(i)); }
 
-        for (var i=97; i<123; i++)
-            AllChars.push(String.fromCharCode(i));
-        
-        return AllChars;
-    }
+    return AllChars;
+  }
 
-    function normalizeString(string){
-        string = string.replace(/[.,\/#!$%\^&\*\[\]@;:{}=\-_`~()]/g,"");
-        return string;
-    }
+  function normalizeString(string) {
+    const result = string.replace(/[.,#!$%^&*[\]@;:{}=\-_`~/()]/g, '');
+    return result;
+  }
 
-    function encrypt(string,step){    
-        string = normalizeString(string); 
-        let symbols = translateToNums();
-        let encryptIndexes = [];
-        for(let i of string){
-            if (i == i.toLowerCase())            
-                symbols = symbols.map((letter)=>letter.toLowerCase());            
-            else            
-                symbols = symbols.map((letter)=>letter.toUpperCase());            
-            let index = (symbols.indexOf(i)+step) % 26;
-            encryptIndexes.push(symbols[index]);
-        }
-         return encryptIndexes.join("");
-    }
+  function encrypt(string, step) {
+    const result = normalizeString(string);
+    let symbols = translateToNums();
+    const encryptIndexes = [];
+    result.split('').forEach((i) => {
+      if (i === i.toLowerCase()) {
+        symbols = symbols.map((letter) => letter.toLowerCase());
+      } else {
+        symbols = symbols.map((letter) => letter.toUpperCase());
+      }
+      const index = (symbols.indexOf(i) + step) % 26;
+      encryptIndexes.push(symbols[index]);
+    });
+    return encryptIndexes.join('');
+  }
 
-    function decrypt(string,step){
-        string = normalizeString(string);
-        let symbols = translateToNums();   
-        let decryptIndexes = [];
-        for(let i of string){
-            if (i == i.toLowerCase())            
-                symbols = symbols.map((letter)=>letter.toLowerCase());            
-            else            
-                symbols = symbols.map((letter)=>letter.toUpperCase());    
-           
-            let index = (symbols.indexOf(i)-step) % 26;
-           
-            if(index < 0 && index != -1)
-                 decryptIndexes.push(symbols[symbols.length+index]);
-            else
-                decryptIndexes.push(symbols[index]);
-        }
-         return decryptIndexes.join("");
-    }
+  function decrypt(string, step) {
+    const result = normalizeString(string);
+    let symbols = translateToNums();
+    const decryptIndexes = [];
+    result.split('').forEach((i) => {
+      if (i === i.toLowerCase()) {
+        symbols = symbols.map((letter) => letter.toLowerCase());
+      } else {
+        symbols = symbols.map((letter) => letter.toUpperCase());
+      }
 
-    return {
-        encrypt,decrypt
-    };
+      const index = (symbols.indexOf(i) - step) % 26;
+
+      if (index < 0 && index !== -1) {
+        decryptIndexes.push(symbols[symbols.length + index]);
+      } else {
+        decryptIndexes.push(symbols[index]);
+      }
+    });
+    return decryptIndexes.join('');
+  }
+
+  return {
+    encrypt, decrypt,
+  };
 })();
 
-function analyze(array){
-    const average = array.reduce((previous,current)=> previous+current)/array.length;
-    const min = array.reduce((previous,current)=> previous < current ? previous : current);
-    const max = array.reduce((previous,current)=> previous > current ? previous : current);
-    const length = array.length;
+function analyze(array) {
+  const average = array.reduce((previous, current) => previous + current) / array.length;
+  const min = array.reduce((previous, current) => (previous < current ? previous : current));
+  const max = array.reduce((previous, current) => (previous > current ? previous : current));
+  const { length } = array;
 
-    return {average,min,max,length};
+  return {
+    average, min, max, length,
+  };
 }
 
-module.exports = {capitalize,reverseString,Calculator,caesarCipher,analyze};
+export {
+  capitalize, reverseString, Calculator, caesarCipher, analyze,
+};
--- testingPractices.test.js
+++ testingPractices.test.js
@@ -1,21 +1,20 @@
-let imp = require('./testingPractice');
-
+import { capitalize, reverseString, Calculator, caesarCipher, analyze } from './testingPractice.js';
 //String Manipulation
 describe('String Manipulation', () => {
     //Capitalize
     test("Capitalize 'something' should return 'Something'", ()=>{
-        expect(imp.capitalize("something")).toEqual("Something");
+        expect(capitalize("something")).toEqual("Something");
     });
 
     //ReverseString
     test("ReverseString 'something' should return 'gnihtemos'", ()=>{
-        expect(imp.reverseString("something")).toEqual("gnihtemos");
+        expect(reverseString("something")).toEqual("gnihtemos");
     });
 });
 
 //Calculator
 describe('Calculator', () => {
-    let calculator = new imp.Calculator(6,3);
+    let calculator = new Calculator(6,3);
 
     test("6 + 3 equals 9", ()=>{
         expect(calculator.add()).toBe(9);
@@ -36,48 +35,47 @@ describe('Calculator', () => {
 
 //Caesar Cipher
 describe('Caesar Cipher', () => {
-    let caesarFunctions = imp.caesarCipher;
     //Encrypt
     //Encrypt string with step 1
     test("Encrypt 'something' with step 1 must become 'tpnfuijoh'", ()=>{
-        expect(caesarFunctions.encrypt("something",1)).toEqual("tpnfuijoh");
+        expect(caesarCipher.encrypt("something",1)).toEqual("tpnfuijoh");
     });
     //Wrapping from z-a
     test("wrapping from z-a", ()=>{
-        expect(caesarFunctions.encrypt("something",8)).toEqual("awumbpqvo");
+        expect(caesarCipher.encrypt("something",8)).toEqual("awumbpqvo");
     });
     //Wrapping from Z-A
     test("wrapping from Z-A", ()=>{
-        expect(caesarFunctions.encrypt("SOMETHING",8)).toEqual("AWUMBPQVO");
+        expect(caesarCipher.encrypt("SOMETHING",8)).toEqual("AWUMBPQVO");
     });
     //Punctuation
     test("Punctuation of 'something!@#$%&*[]{}:;=-~()' with step 1 to become 'tpnfuijoh",()=>{
-        expect(caesarFunctions.encrypt("something!@#$%&*[]{}:;=-~()",1)).toEqual("tpnfuijoh");
+        expect(caesarCipher.encrypt("something!@#$%&*[]{}:;=-~()",1)).toEqual("tpnfuijoh");
     });
     //Punctuation , wrapping from a-z, wrapping from A-Z
     test("Normalize 'SoMeThIng!@#$%&*[]{}:;=-~()' to become 'AwUmBpQvo'", ()=>{
-        expect(caesarFunctions.encrypt("SoMeThIng!@#$%&*[]{}:;=-~()",8)).toEqual("AwUmBpQvo");
+        expect(caesarCipher.encrypt("SoMeThIng!@#$%&*[]{}:;=-~()",8)).toEqual("AwUmBpQvo");
     });
     //Decrypt
     //Decrypt string with step 1
     test("Decrypt 'tpnfuijoh' with step 1 must become 'something'", ()=>{
-        expect(caesarFunctions.decrypt("tpnfuijoh",1)).toEqual("something");
+        expect(caesarCipher.decrypt("tpnfuijoh",1)).toEqual("something");
     });
     //Wrapping from z-a
     test("wrapping from z-a", ()=>{
-        expect(caesarFunctions.decrypt("awumbpqvo",8)).toEqual("something");
+        expect(caesarCipher.decrypt("awumbpqvo",8)).toEqual("something");
     });
     //Wrapping from Z-A
     test("wrapping from Z-A", ()=>{
-        expect(caesarFunctions.decrypt("AWUMBPQVO",8)).toEqual("SOMETHING");
+        expect(caesarCipher.decrypt("AWUMBPQVO",8)).toEqual("SOMETHING");
     });
     //Punctuation
     test("Punctuation of 'tpnfuijoh!@#$%&*[]{}:;=-~()' with step 1 to become 'something' ",()=>{
-        expect(caesarFunctions.decrypt("tpnfuijoh!@#$%&*[]{}:;=-~()",1)).toEqual("something");
+        expect(caesarCipher.decrypt("tpnfuijoh!@#$%&*[]{}:;=-~()",1)).toEqual("something");
     });
     //Punctuation , wrapping from a-z, wrapping from A-Z
     test("Normalize 'AwUmBpQvo!@#$%&*[]{}:;=-~()' to become 'SoMeThIng'", ()=>{
-        expect(caesarFunctions.decrypt("AwUmBpQvo!@#$%&*[]{}:;=-~()",8)).toEqual("SoMeThIng");
+        expect(caesarCipher.decrypt("AwUmBpQvo!@#$%&*[]{}:;=-~()",8)).toEqual("SoMeThIng");
     });
  
 
@@ -87,7 +85,7 @@ describe('Caesar Cipher', () => {
 //Array Analyzing
 describe('Analyze Array', ()=>{
     let array = [1,2,3,4,5];
-    let result = imp.analyze(array);
+    let result = analyze(array);
 
     test("[1,2,3,4,5] average value should be 3",()=>{
         expect(result.average).toBe(3);
